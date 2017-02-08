b8e91ea1b9ae4f64fe37e1faf375830f51fd8425
Kapunahele Wong <kapunahelewong@gmail.com>
2017-02-08 12:23:30 -0500

    Add expandable headers to sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -6,11 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  greeting = true;
+  section1 = false;
+  section2 = false;
+  section3 = false;
+
+  toggleSection1() {
+    this.section1 = !this.section1;
+  }
+
+  toggleSection2() {
+    this.section2 = !this.section2;
+  }
+
+  toggleSection3() {
+    this.section3 = !this.section3;
+  }
+
+  toggleGreeting() {
+    this.greeting = !this.greeting;
+  }
+
+
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
 
