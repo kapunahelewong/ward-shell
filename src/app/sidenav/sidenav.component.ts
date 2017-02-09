e2a0e32395602d7b6c523007b765f32c2c964877
Kapunahele Wong <kapunahelewong@gmail.com>
2017-02-09 12:20:22 -0500

    Add three tiers to nav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -8,13 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  isSideNavOpen: boolean;
   section1 = false;
+  section1b = false;
+  section1c = false;
   section2 = false;
   section3 = false;
 
+  toggleSideNav() {
+
+  }
   toggleSection1() {
     this.section1 = !this.section1;
   }
+  toggleSection1b() {
+    this.section1b = !this.section1b;
+  }
+  toggleSection1c() {
+    this.section1c = !this.section1c;
+  }
+
 
   toggleSection2() {
     this.section2 = !this.section2;
